1a3379bc9a3bba05be0088709d633c9c58b93d83
Mahesh Paul <123456magimagi@gmail.com>
2024-12-08 12:30:31 +0530

    fix: vercel fix

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const { Server } = require('socket.io');
 const path = require('path');
 
-const port = process.env.PORT || 3001;
+const port = 3001;
 
 // Create Express app
 const app = express();
@@ -12,7 +12,7 @@ const server = http.createServer(app);
 // Configure Socket.IO
 const io = new Server(server, {
   cors: {
-    origin: ['https://canva-backend-n71m.onrender.com/'],
+    origin: ['https://canva-backend-mu.vercel.app/'],
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   },
